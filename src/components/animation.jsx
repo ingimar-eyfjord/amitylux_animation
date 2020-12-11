3153a449374524a02fec703bd9d258bb9fa2f82a
Ingimar <ingimareys93@gmail.com>
2020-12-11 13:38:34 +0100

    for Indre's review

---
+++ src/components/animation.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import animationData from "./logo_animation.json";
+import lottie from "lottie-web";
+
+class LogoAnimated extends Component {
+    constructor(props) {
+        super(props)
+        this.container = React.createRef();
+
+    }
+    componentDidMount() {
+        const animation = lottie.loadAnimation({
+            container: this.container.current,
+            renderer: "svg",
+            loop: false,
+            autoplay: false,
+            animationData: animationData,
+        });
+
+        animation.play();
+        animation.addEventListener("complete", () => {
+            setTimeout(() => {
+                animation.stop();
+                animation.play();
+            }, 10000);
+        });
+    }
+
+    render() {
+        return (
+
+            <div className="logo" ref={this.container}></div>
+
+        )
+    }
+
+}
+
+export default LogoAnimated;
+
